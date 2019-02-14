6516bd1ae0d2eef04085761f77c8ca8ec5402353
Akshay Jain <45626@hexaware.com>
2019-02-14 12:43:12 +0530

    adding lodash and recompose

--- src/Map.js
+++ src/Map.js
@@ -6,8 +6,6 @@ import {
   Marker,
   withScriptjs
 } from 'react-google-maps';
-// import withScriptjs from "react-google-maps/lib/async/withScriptjs";
-// import DrawingManager from "react-google-maps/lib/drawing/DrawingManager";
 import DrawingManager from "react-google-maps/lib/components/drawing/DrawingManager"
 
 export default withScriptjs(withGoogleMap(props => (
