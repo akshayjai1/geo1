332da7dbe0a53a735ab5f342cde870d0bb8236a3
Akshay Jain <45626@hexaware.com>
2019-02-18 14:44:28 +0530

    added react dev tools extension

--- src/CreateStore.js
+++ src/CreateStore.js
@@ -5,7 +5,9 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 
 export const middlewares = [ReduxThunk];
 
-export const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore);
+// export const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore);
+const enhancer = composeWithDevTools(applyMiddleware(...middlewares));
 
-export const store = createStoreWithMiddleware(RootReducer);
+// export const store = createStoreWithMiddleware(RootReducer);
+export const store = createStore(RootReducer,{},enhancer);
 console.log('store created successfully, this is current state in store', store.getState());
