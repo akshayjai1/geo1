46122daca6c96f1086b3436f4bcf1e963fff642e
Akshay Jain <45626@hexaware.com>
2019-02-18 19:02:11 +0530

    adding routing to app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React, {Component} from 'react';
 import Moment from 'react-moment';
 import { connect } from 'react-redux';
+import {BrowserRouter, Route, Switch, Redirect, NavLink } from 'react-router-dom';
 import Map from './Map';
 import './App.css';
 import MapWithASearchBox from './MapWithSearchBox';
@@ -16,15 +17,26 @@ class App extends Component {
     console.log('this is props of App',props);
   }
 
-
   render() {
     console.log('inside the render method of app, this is props', this.props);
     return (
       <div className="App">
-        <GeoMap />
+        <BrowserRouter>
+          <React.Fragment>
+            <ul>
+              <NavLink to="/geoFence"><li>GeoFence</li></NavLink>
+              <NavLink to="/geoSearch"><li>Search</li></NavLink>
+            </ul>
+          <Switch>
+            <Route path="/geoFence" component={GeoMap} />
+            <Route path="/geoSearch" component={MapWithASearchBox} />
+          </Switch>
+          </React.Fragment>
+        </BrowserRouter>
       </div>
     );
   }
 }
 
 export default App;
+{/* <Redirect from="/" to="/geoFence" /> */}
