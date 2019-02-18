ecedb19986ad2a551ded652c11d81931e5f68941
Akshay Jain <45626@hexaware.com>
2019-02-18 15:19:24 +0530

    added trace to devtool extension

--- src/CreateStore.js
+++ src/CreateStore.js
@@ -3,11 +3,16 @@ import ReduxThunk from 'redux-thunk';
 import RootReducer from './reducers';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
+const devToolsOption = {trace: true, name: "Inchcape Shipping"}
+const enhancedDevTools = composeWithDevTools(devToolsOption);
+
 export const middlewares = [ReduxThunk];
+const enhancer = enhancedDevTools(applyMiddleware(...middlewares));
 
-// export const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore);
-const enhancer = composeWithDevTools(applyMiddleware(...middlewares));
-
-// export const store = createStoreWithMiddleware(RootReducer);
 export const store = createStore(RootReducer,{},enhancer);
 console.log('store created successfully, this is current state in store', store.getState());
+
+
+
+// export const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore);
+// export const store = createStoreWithMiddleware(RootReducer);
